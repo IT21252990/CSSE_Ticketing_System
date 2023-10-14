0b9333036170a618984a8465e44c8250d1fd0788
KalingaDJ <89300552+IT21252990@users.noreply.github.com>
2023-10-14 16:09:27 +0530

    done some pages of passenger

---
+++ frontend/Passenger/constants/colors.js
@@ -0,0 +1,11 @@
+// colours
+const Colors = {
+    primary: '#E0E0E0' ,
+    secondary: '#FF9800' ,
+    tertinary: '#E19B33',
+    lightOrange: '#F5C681',
+    darkGray: '#434448',
+    red: '#F9756C'
+}
+
+export default Colors ;
